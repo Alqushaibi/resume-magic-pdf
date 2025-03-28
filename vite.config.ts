a1f00f4e41725f5782da49244ab576aff823035c
Alawi Alqushaibi <alawi.alqushaibi@gmail.com>
2025-03-29 01:13:04 +0800

    d

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["66ff6c47-be0b-4e25-9892-7942e325bfa9.lovableproject.com"], // Add this line
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
